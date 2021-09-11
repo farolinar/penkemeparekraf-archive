c7794530b3c20f858010359ab8ebddf420e3ca75
Farolina Rahmatunnisa <farolina@moselo.com>
2021-09-11 18:21:08 +0700

    Footer

--- src/js/views/layout/footer.js
+++ src/js/views/layout/footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
 import "./layout.scss";
+import icWa from "../../../assets/img/icons/ic-wa.png";
+import icEmail from "../../../assets/img/icons/ic-email.png";
 
 const Footer = () => {
-    return(<div>
-        
+    return(<div className="footer-wrapper">
+        <h2>Kontak Kami</h2>
+        <p>Hubungi kontak person dibawah ini untuk terhubung dengan kami</p>
+        <div className="footer-contact-wrapper">
+            <div className="footer-contact">
+                <LazyLoad>
+                    <img src={icEmail} alt="email" />
+                </LazyLoad>
+                <a href="mailto:penfilm@kemenparekraf.go.id" target="_blank">penfilm@kemenparekraf.go.id</a>
+            </div>
+            <div className="footer-contact">
+                <LazyLoad>
+                    <img src={icWa} alt="wa" />
+                </LazyLoad>
+                <a href="https://wa.me/6288888888888" target="_blank">0888 8888 8888</a>
+            </div>
+        </div>
     </div>)
 }
 
