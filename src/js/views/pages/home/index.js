1052901a12207389c2e0a03f9b53db618a0fd69d
Farolina Rahmatunnisa <farolina@moselo.com>
2021-09-11 12:29:24 +0700

    remove unused import

--- src/js/views/pages/home/index.js
+++ src/js/views/pages/home/index.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect, useState, lazy } from "react";
-import "./layout.scss";
 const Template = lazy(()=>import('../../layout/template'));
 
 const Home = (props) => {
