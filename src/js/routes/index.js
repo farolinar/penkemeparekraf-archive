700b53796c7ef65e4a148ec6b0adfe99589be3e4
Farolina Rahmatunnisa <farolina@moselo.com>
2021-09-12 10:42:54 +0700

    About us page

--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -2,11 +2,18 @@
 import { lazy } from "react";
 import { ROUTES } from "../constants";
 const Home = lazy(()=>import('../views/pages/home'));
+const AboutUs = lazy(()=>import('../views/pages/aboutUs'));
 
 const Routes = [
     {
         path: ROUTES.home,
         component: Home,
+        exact: true,
+        default: true
+    },
+    {
+        path: ROUTES.aboutUs,
+        component: AboutUs,
         exact: true,
         default: true
     },
---
+++ src/js/views/pages/aboutUs/index.js
@@ -0,0 +1,43 @@
+import React, { lazy } from 'react';
+import LazyLoad from 'react-lazyload';
+import Template from '../../layout/template';
+import aboutUsText from '../../../../assets/img/pages/aboutUs/about-us-text.png';
+import aboutUsHero from '../../../../assets/img/pages/aboutUs/about-us-hero.jpg';
+import aboutUsBg from '../../../../assets/img/pages/aboutUs/about-us-bg.jpg';
+import aboutUsSelection from '../../../../assets/img/pages/aboutUs/about-us-selection-clean.png';
+import "./AboutUs.scss";
+
+const AboutUs = () => {
+    return(<Template>
+        <div className="about-us-main-wrapper">
+            <h2>Tentang Kami</h2>
+            <div className="about-us-content">
+                <div>
+                    <img src={aboutUsText} alt="about-us" />
+                </div>
+                <LazyLoad>
+                    <img src={aboutUsHero} alt="about-us" />
+                </LazyLoad>
+            </div>
+        </div>
+
+        <div className="about-us-selection-wrapper">
+            {/* <div className="about-us-bg">
+                <LazyLoad>
+                    <img src={aboutUsBg} alt="about-us" />
+                </LazyLoad>
+            </div> */}
+            <div className="about-us-bg-filter"></div>
+            <h2>Alur Seleksi Proposal Program PEN Film</h2>
+            <div className="about-us-selection-content-wrapper">
+                <div className="about-us-selection-content">
+                    <LazyLoad>
+                        <img src={aboutUsSelection} alt="about-us" />
+                    </LazyLoad>
+                </div>
+            </div>
+        </div>
+    </Template>)
+}
+
+export default AboutUs;
