686e089b32a858d13487f1342301bff0b9b54495
Farolina Rahmatunnisa <farolina@moselo.com>
2021-10-07 22:00:38 +0700

    Text length validation rumah produksi

--- src/js/views/component/textAreaWithCounter/index.js
+++ src/js/views/component/textAreaWithCounter/index.js
@@ -6,9 +6,11 @@ const TextAreaWithCounter = ({
     value = "",
     onChange = () => {},
     maxLength=false,
+    minLength=false,
     rows="10"
 }) => {
     const [numberOfChars, setNumberOfChars] = useState(0)
+    const [validation, setValidation] = useState("")
     useEffect(() => {
         setNumberOfChars(value.length);
     }, [value])
@@ -17,13 +19,19 @@ const TextAreaWithCounter = ({
             name={name}
             defaultValue={value}
             onChange={(e) => {
-                onChange(e)
+                let msg = ""
+                if(minLength && e.target.value.length < minLength) {
+                    msg = `Panjang teks minimal ${minLength} karakter`
+                }
+                setValidation(msg)
+                onChange(e, msg)
                 // setNumberOfChars(e.target.value.length);
             }}
             maxLength={maxLength}
             rows={rows}
         ></textarea>
-        {maxLength && <span>{numberOfChars}/{maxLength}</span>}
+        {validation && <span className="info-left">{validation}</span>}
+        {maxLength && <span className="info-right">{numberOfChars}/{maxLength}</span>}
     </div>)
 }
 
