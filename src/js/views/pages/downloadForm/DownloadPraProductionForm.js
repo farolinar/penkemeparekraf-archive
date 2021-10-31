419c42db8d149b36a1073cef2c0f545fdfc53055
Farolina Rahmatunnisa <farolina@moselo.com>
2021-10-31 16:34:58 +0700

    Update download link

--- src/js/views/pages/downloadForm/DownloadPraProductionForm.js
+++ src/js/views/pages/downloadForm/DownloadPraProductionForm.js
@@ -6,31 +6,31 @@ import "./DownloadForm.scss";
 const MAIN_FORMS = [
     {
         name: "Unduh Lampiran 1 - Format Surat Pernyataan Rumah Produksi",
-        url: "https://drive.google.com/uc?export=download&id=1cqpo_dWV_vuRl9jciSqJMo9o5FKjcsxk"
+        url: "https://drive.google.com/uc?export=download&id=1Ftjogc-FhwoUUa1jc38ZFUBEmIbhrpu2"
     },
     {
         name: "Unduh Lampiran 2 - Format Surat Pernyataan Tanggung Jawab Mutlak",
-        url: "https://drive.google.com/uc?export=download&id=1S9pxFKemz_6kHY2BomldU-AvD-C8vwKy"
+        url: "https://drive.google.com/uc?export=download&id=1FWSLges3SAERYgX2v1uvKeIBfwjoel-6"
     },
     {
         name: "Unduh Lampiran 3 - Format Surat Pernyataan Tidak Menerima Bantuan Pemerintah PEN-Film Skema Lainnya dan/atau Bantuan K/L Lainnya",
-        url: "https://drive.google.com/uc?export=download&id=1oNmtqqZ0Q15kEmlnQNmcSbbQvmLuw77m"
+        url: "https://drive.google.com/uc?export=download&id=13RYdqNuIvIvXimT2Ma52PxFQjvPhRnQS"
     },
     {
         name: "Unduh Lampiran 4 - Format Surat Permohonan Bantuan Pemerintah Pra-Produksi Film Indonesia",
-        url: "https://drive.google.com/uc?export=download&id=1XhjMpvLgDek68IwCxYPomviaMKY8NB2V"
+        url: "https://drive.google.com/uc?export=download&id=1fx-OeaospU01FS9vKtl-wQUR5fWRI9T4"
     },
     {
         name: "Unduh Lampiran 5 - Format Ringkasan Profil Pengusul Proposal Permohonan Bantuan Pemerintah Pra-Produksi Film Indonesia",
-        url: "https://drive.google.com/uc?export=download&id=1l_NtcGD9MYuHJ5hifaFXDIJnpo7ssK7f"
+        url: "https://drive.google.com/uc?export=download&id=1qX5adrBjFhkx_BkAKEAlwN76yOzJsQQy"
     },
     {
         name: "Unduh Lampiran 6 - Format Proposal Bantuan Pemerintah Pra-Produksi Film Indonesia",
-        url: "https://drive.google.com/uc?export=download&id=1OORvmSTVYLD0nkwGXFUO1MlOwRQYEuz4"
+        url: "https://drive.google.com/uc?export=download&id=104rTEdj47SxRGSF1j4qxQO_vsBI3R1yL"
     },
     {
         name: "Unduh Lampiran 7 - Format Rincian Anggaran Biaya (RAB) Bantuan Pemerintah Pra-Produksi Film Indonesia",
-        url: "https://drive.google.com/uc?export=download&id=1xh7fFIPr-zJBRXZdRxgnnii_Rc-kiYnH"
+        url: "https://drive.google.com/uc?export=download&id=12IhVM1fF4MP1qM5QW_eC_7mhvA7bouei"
     },
 ]
 
