036a27c439c246ac627852626eba4a09b4939f92
Farolina Rahmatunnisa <farolina@moselo.com>
2021-11-07 23:14:27 +0700

    Build

---
+++ build/static/js/28.599a0db7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpweb-pen"]=this["webpackJsonpweb-pen"]||[]).push([[28],{40:function(n,e,t){"use strict";t.r(e);var c=t(0),i=(t(41),t(1)),u=Object(c.lazy)((function(){return t.e(26).then(t.bind(null,43))})),s=Object(c.lazy)((function(){return t.e(17).then(t.bind(null,42))}));e.default=function(n){return Object(i.jsxs)("div",{className:"app",children:[Object(i.jsx)(u,{}),n.children,!n.withoutFooter&&Object(i.jsx)(s,{})]})}},41:function(n,e,t){}}]);
+//# sourceMappingURL=28.599a0db7.chunk.js.map
