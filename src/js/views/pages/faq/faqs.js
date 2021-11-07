2a6868fde4f099576c9fb5a7d24e46fdc86529fd
Dominic Vedericho <>
2021-11-07 12:07:42 +0700

    change wording FAQ, build

--- src/js/views/pages/faq/faqs.js
+++ src/js/views/pages/faq/faqs.js
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 export const FAQs = [
     {
         title: "Apakah yang mengajukan pendaftaran harus menduduki jabatan sebagai direksi?",
-        content: <p>Pihak yang mendaftar adalah Rumah Produksi dan yang bertanggungjawab adalah orang yang secara hukum sah mewakili Rumah Produksi sesuai dengan Akta dan/atau AD/ART Rumah Produksi/selevel direksi dan merupakan Warga Negara Indonesia yang dibuktikan dengan kepemilikan Kartu Tanda Penduduk."</p>
+        content: <p>Pihak yang mendaftar adalah Rumah Produksi dan yang bertanggungjawab adalah orang yang secara hukum sah mewakili Rumah Produksi sesuai dengan Akta dan/atau AD/ART Rumah Produksi/selevel direksi dan merupakan Warga Negara Indonesia yang dibuktikan dengan kepemilikan Kartu Tanda Penduduk</p>
     },
     {
         title: "Dalam hal pengajuan proposal, apakah urutan harus sesuai yang tercantum dalam persyaratan?",
@@ -22,7 +22,7 @@ export const FAQs = [
                 <li>Detail rencana dan timeline produksi proyek film yang diajukan</li>
                 <li>Portofolio produksi film selama tahun 2020-2021</li>
             </ul>
-            <p>Proposal dibuat dalam bentuk PDF dengan urutan yang benar dan lengkap serta halaman muka yang merujuk dengan jelas judul film.</p>
+            <p>Proposal dibuat dalam bentuk PDF dengan urutan yang benar dan lengkap serta halaman muka yang merujuk dengan jelas judul film</p>
         </Fragment>
     },
     {
@@ -31,18 +31,23 @@ export const FAQs = [
     },
     {
         title: "Untuk Bantuan Pemerintah Pra-Produksi Film Indonesia, berapa durasi yang dipersyaratkan untuk Film Panjang dan/atau Film Dokumenter Panjang?",
+        content:
+            <p>Minimal 80 menit</p>
+    },
+    {
+        title: "Bagaimana cara Rumah Produksi memastikan bahwa berkas administrasi telah sesuai dengan ketentuan yang ada di dalam pemerintahan?",
         content: <Fragment>
-            <p>Rumah Produksi dapat menggunakan standar biaya masukan pemerintah, dengan melihat Peraturan Menteri Keuangan RI, Nomor 119/PMK.02/2020 (SBM 2021).</p>
+            <p>Rumah Produksi dapat menggunakan standar biaya masukan pemerintah, dengan melihat Peraturan Menteri Keuangan RI, Nomor 119/PMK.02/2020 (SBM 2021)</p>
             <br />
-            <p>Atau Rumah Produksi dapat menggunakan “jasa konsultan keuangan” untuk memastikan berkas pertanggungjawaban administrasi dan keuangan lengkap dan sesuai dengan aturan yang ada dalam pemerintahan.</p>
+            <p>Atau Rumah Produksi dapat menggunakan “jasa konsultan keuangan” untuk memastikan berkas pertanggungjawaban administrasi dan keuangan lengkap dan sesuai dengan aturan yang ada dalam pemerintahan</p>
         </Fragment>
     },
     {
         title: "Bagaimana cara Rumah Produksi mempertanggung jawabkan penggunaan dana Bantuan Pemerintah ini?",
-        content: <p>Pertanggungjawaban disampaikan melalui laporan kegiatan dan keuangan yang sesuai dengan peruntukan dana yang dipergunakan. Adapun isi laporan antara lain (laporan kegiatan tertulis, rincian kegiatan, bukti kegiatan dokumentasi detail sesuai rincian, dan kwitansi bukti pembayaran.</p>
+        content: <p>Pertanggungjawaban disampaikan melalui laporan kegiatan dan keuangan yang sesuai dengan peruntukan dana yang dipergunakan. Adapun isi laporan antara lain laporan kegiatan tertulis, rincian kegiatan, bukti kegiatan dokumentasi detail sesuai rincian, dan kwitansi bukti pembayaran</p>
     },
     {
         title: "Apakah portfolio yang dicantukan dalam proposal hanya terbatas di tahun 2020-2021?",
-        content: <p>Rumah Produksi yang berhak mendaftar adalah badan usaha berbentuk Perseroan Terbatas (PT) sesuai dengan ketentuan perundang-undangan yang berlaku dan terdaftar di Kementerian Hukum dan HAM Republik Indonesia serta sudah berdiri sebelum tanggal 1 Januari 2020. Portfolio bisa sebelum 1 Januari 2020.</p>
+        content: <p>Rumah Produksi yang berhak mendaftar adalah badan usaha berbentuk Perseroan Terbatas (PT) sesuai dengan ketentuan perundang-undangan yang berlaku dan terdaftar di Kementerian Hukum dan HAM Republik Indonesia serta sudah berdiri sebelum tanggal 1 Januari 2020. Portfolio bisa sebelum 1 Januari 2020</p>
     }
 ]
